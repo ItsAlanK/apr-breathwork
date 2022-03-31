649998d21368fc001fc9cbbbca9fd8284c880d54
ItsAlanK <alankinsella1993@gmail.com>
2022-03-31 20:30:36 +0100

    Added unit tests for homepage

---
+++ apr_breathwork/home/test_views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+
+class TestViews(TestCase):
+    """ Class for testing home app views """
+
+
+    def test_get_homepage_response(self):
+        """Test response for homepage """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_check_homepage_templates(self):
+        """ Test homepage uses correct templates """
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home/index.html')
