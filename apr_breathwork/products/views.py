92cb71755be46659ca4e1f50a7692f577ffb643d
ItsAlanK <alankinsella1993@gmail.com>
2022-04-06 20:37:36 +0100

    Create urls, basic view and template for products page

--- apr_breathwork/products/views.py
+++ apr_breathwork/products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def products(request):
+    """ View which returns products list """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
