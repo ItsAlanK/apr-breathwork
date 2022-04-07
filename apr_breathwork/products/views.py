97de9752284ed48bc415d325be1d41c140028b98
ItsAlanK <alankinsella1993@gmail.com>
2022-04-07 15:57:46 +0100

    Add filtering by search query to products view and set action for search bar

--- apr_breathwork/products/views.py
+++ apr_breathwork/products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -7,9 +9,20 @@ def products(request):
     """ View which returns products list """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No search query entered.")
+                return redirect(reverse('products'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
