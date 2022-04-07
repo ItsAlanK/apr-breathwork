268175e6ce8865b81e4c6dafaa679cc60a256a80
ItsAlanK <alankinsella1993@gmail.com>
2022-04-07 18:49:15 +0100

    Add places sold as readonly field in admin so it can be viewed

--- apr_breathwork/products/admin.py
+++ apr_breathwork/products/admin.py
@@ -30,8 +30,10 @@ class ProductVariantAdmin(admin.ModelAdmin):
         'date',
         'time',
         'attendance_limit',
+        'places_sold'
     )
 
+    readonly_fields=('places_sold',)
     ordering = ('date', 'time')
 
 
