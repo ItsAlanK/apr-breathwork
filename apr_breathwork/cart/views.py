35db67469787ebdd3d04e038bd080a02e9fdac4f
ItsAlanK <alankinsella1993@gmail.com>
2022-04-09 13:16:15 +0100

    Add urls and template for cart app

--- apr_breathwork/cart/views.py
+++ apr_breathwork/cart/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def view_cart(request):
+    """ View which returns cart contents page """
+    return render(request, 'cart/cart.html')
