aeb67be5f57ffac15da79a1fea1fe9e463c91374
ItsAlanK <alankinsella1993@gmail.com>
2022-04-09 17:57:05 +0100

    Add bag contents to context processor and test in cart.html

--- apr_breathwork/cart/contexts.py
+++ apr_breathwork/cart/contexts.py
@@ -1,10 +1,25 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def cart_contents(request):
+    """ Context processor makes cart contents available across site """
 
+    cart = request.session.get('cart', {})
     cart_items = []
     total = 0
     product_count = 0
+
+    for item_id, variants in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        amount = len(variants)
+        total += product.price * amount
+        cart_items.append({
+            'item_id': item_id,
+            'amount': amount,
+            'product': product,
+            'variants': variants,
+        })
 
     context = {
         'cart_items': cart_items,
--- apr_breathwork/cart/views.py
+++ apr_breathwork/cart/views.py
@@ -26,5 +26,4 @@ def add_to_cart(request, item_id):
         cart[item_id] = [variant]
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
