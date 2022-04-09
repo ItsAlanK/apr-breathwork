713bba7a4a017098c3a3dfedec8cbe03e64b848d
ItsAlanK <alankinsella1993@gmail.com>
2022-04-09 16:44:18 +0100

    Move date/time selector js to static folder

---
+++ apr_breathwork/static/js/date-time-selector.js
@@ -0,0 +1,25 @@
+ // Fixes Duplicate dates when more than one time available for date//
+    // https://stackoverflow.com/questions/22905769/remove-duplicate-options-from-html-select //
+    var usedNames = {};
+    $("select[name='date'] > option").each(function () {
+        if(usedNames[this.text]) {
+            $(this).remove();
+        } else {
+            usedNames[this.text] = this.value;
+        }
+    });
+
+    // Listens for selected date and shows/hides appropriate times //
+    var dateSelector = document.getElementById("date");
+    var timeSelector = document.getElementById("time");
+    dateSelector.addEventListener("change", function() {
+        chosenDate = dateSelector.options[dateSelector.selectedIndex].value;
+        for (var i=0; i<timeSelector.length; i++) {
+            if (timeSelector.options[i].id == chosenDate) {
+                timeSelector.options[i].classList.remove("d-none");
+            }
+            else {
+                timeSelector.options[i].classList.add("d-none");
+            }
+        }
+    });
