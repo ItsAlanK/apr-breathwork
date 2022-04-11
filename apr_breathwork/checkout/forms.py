bc2099cb33b577e5e770d4370082b343923cb5e2
ItsAlanK <alankinsella1993@gmail.com>
2022-04-11 20:48:27 +0100

    Add signals to update order total as line items are added and create form for orders

---
+++ apr_breathwork/checkout/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+
+
+class OrderForm(forms.ModelForm):
+    """ Set model and fields for order form """
+    class Meta:
+        """ Set model and fields """
+        model = OrderForm
+        fields = (
+            "full_name", "email",
+            "phone_number",
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
