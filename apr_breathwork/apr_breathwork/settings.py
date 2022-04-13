5547258ad5000b7f759a50803ef312c456cf0d0d
ItsAlanK <alankinsella1993@gmail.com>
2022-04-13 20:02:10 +0100

    Create profile app and set up models template view and url for profile page

--- apr_breathwork/apr_breathwork/settings.py
+++ apr_breathwork/apr_breathwork/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+    'profiles',
     'crispy_forms',
 ]
 
---
+++ apr_breathwork/profiles/models.py
@@ -0,0 +1,29 @@
+import datetime
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    delivery information and order history
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    paid_member_from = models.DateField(default=datetime.date.today)
+    is_paid_member = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
