0c045170a03c547c03f764a5dbfa78ed5aba6ac4
ItsAlanK <alankinsella1993@gmail.com>
2022-04-13 20:02:10 +0100

    Create profile app and set up models template view and url for profile page

--- apr_breathwork/checkout/models.py
+++ apr_breathwork/checkout/models.py
@@ -3,12 +3,16 @@ from django.db import models
 from django.conf import settings
 from django.db.models import Sum
 from products.models import Product, ProductVariant
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     """ Model holding order info """
 
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL,
+        null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
