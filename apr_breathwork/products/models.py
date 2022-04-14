aeb73ca84ebe4337d8228e9240d555b6d20c2b5c
ItsAlanK <alankinsella1993@gmail.com>
2022-04-14 21:48:20 +0100

    Changed timescale for product duration from hrs to mins for clarity

--- apr_breathwork/products/models.py
+++ apr_breathwork/products/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    duration = models.DecimalField(max_digits=3, decimal_places=2)
+    duration = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
