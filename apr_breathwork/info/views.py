ba4e9235cc52cd7299b47153bc60c91708e1c6ae
ItsAlanK <alankinsella1993@gmail.com>
2022-04-16 21:02:30 +0100

    Add content to about us template

--- apr_breathwork/info/views.py
+++ apr_breathwork/info/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import AboutUs
 
 def about(request):
     """ View which returns index page """
-    return render(request, 'info/about-us.html')
+
+    about_us = get_object_or_404(AboutUs, pk=1)
+
+    context = {
+        'about_us': about_us,
+    }
+    template = 'info/about-us.html'
+    return render(request, template, context)
