eaf5671f5f2bba182d9116939cbaf5cc73419c47
ItsAlanK <alankinsella1993@gmail.com>
2022-04-16 23:37:58 +0100

    Charge path to settings trying to fix deploy crash on heroku

--- apr_breathwork/apr_breathwork/wsgi.py
+++ apr_breathwork/apr_breathwork/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apr_breathwork.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apr_breathwork.apr_breathwork.settings')
 
 application = get_wsgi_application()
