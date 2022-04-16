23cef9e0ce1d6bfe264dc8b0413664767c6ac8f3
ItsAlanK <alankinsella1993@gmail.com>
2022-04-16 22:17:04 +0100

    Deployment setup continued

--- apr_breathwork/apr_breathwork/settings.py
+++ apr_breathwork/apr_breathwork/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-1gt(#(pb*@w901^^1i3p5&hao%w*(a7c7ucte0tu9$1cv3g&v_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 1
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['apr-breathwork.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,12 +125,17 @@ WSGI_APPLICATION = 'apr_breathwork.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
