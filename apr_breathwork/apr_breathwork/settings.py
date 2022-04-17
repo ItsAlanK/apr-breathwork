a2ab6d10bab01eeb2794a63285b57b9205b60b7c
ItsAlanK <alankinsella1993@gmail.com>
2022-04-17 16:30:40 +0100

    Correct typo in in settings file storage section for aws

--- apr_breathwork/apr_breathwork/settings.py
+++ apr_breathwork/apr_breathwork/settings.py
@@ -201,7 +201,7 @@ if 'USE_AWS' in os.environ:
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storage.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     #Override static and media URLs in production
