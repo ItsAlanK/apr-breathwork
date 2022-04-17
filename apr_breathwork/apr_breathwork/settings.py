7b9e46218d467f561c064805a539ef6b4eba6d2f
ItsAlanK <alankinsella1993@gmail.com>
2022-04-17 20:20:50 +0100

    Add settings from gmail hosting

--- apr_breathwork/apr_breathwork/settings.py
+++ apr_breathwork/apr_breathwork/settings.py
@@ -111,8 +111,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -225,3 +223,15 @@ CKEDITOR_CONFIGS = {
         'toolbar': None,
     },
 }
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'aprbreathwork@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_POST = 587
+    EMAIL_HOST = 'smpt.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
