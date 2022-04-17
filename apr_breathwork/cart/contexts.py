67e213b7e93975f5d01facae4c193ccb9167ff10
ItsAlanK <alankinsella1993@gmail.com>
2022-04-17 16:55:00 +0100

    Fix bug where context processor only gets variant based on data and time but not product

--- apr_breathwork/cart/contexts.py
+++ apr_breathwork/cart/contexts.py
@@ -25,7 +25,7 @@ def cart_contents(request):
             date, time = split_variant[0],split_variant[1]
             formatted_date = datetime.strptime(date, '%B %d, %Y')
             formatted_time = datetime.strptime(time, '%H:%M')
-            variant_selected = get_object_or_404(ProductVariant, date=formatted_date, time=formatted_time)
+            variant_selected = get_object_or_404(ProductVariant, product=product, date=formatted_date, time=formatted_time)
             dates += [date]
             times += [time]
             variants_selected += [variant_selected]
