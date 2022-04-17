9eacc94f2318493befc77212fd226e8728ee7b00
ItsAlanK <alankinsella1993@gmail.com>
2022-04-17 20:48:45 +0100

    Fix typo in email settings

--- apr_breathwork/apr_breathwork/settings.py
+++ apr_breathwork/apr_breathwork/settings.py
@@ -233,5 +233,5 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smpt.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
