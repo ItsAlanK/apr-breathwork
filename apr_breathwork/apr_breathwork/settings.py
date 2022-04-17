f23222dde7aa2a5942a566933c7906dfc939f9e3
ItsAlanK <alankinsella1993@gmail.com>
2022-04-17 11:53:58 +0100

    Add cache rules to aws settings

--- apr_breathwork/apr_breathwork/settings.py
+++ apr_breathwork/apr_breathwork/settings.py
@@ -184,6 +184,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # AWS Bucket config
     AWS_STORAGE_BUCKET_NAME = 'apr-breathwork'
     AWS_S3_REGION_NAME = 'eu-west-1'
