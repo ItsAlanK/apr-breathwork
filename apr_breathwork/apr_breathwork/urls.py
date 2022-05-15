a3109ed923f4e75a72c23d55ba6f6fc90fbbe675
ItsAlanK <alankinsella1993@gmail.com>
2022-05-15 21:53:53 +0100

    Add robots.txt file to site disallowing accounts and cart pages for crawling

--- apr_breathwork/apr_breathwork/urls.py
+++ apr_breathwork/apr_breathwork/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic.base import TemplateView
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -28,6 +29,7 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('course/', include('course.urls')),
     path('info/', include('info.urls')),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404='home.views.page_not_found_handler'
