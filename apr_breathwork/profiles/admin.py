3c0a159e6bc04e40a69b4da5e4093dc7aca8b222
ItsAlanK <alankinsella1993@gmail.com>
2022-05-16 21:56:12 +0100

    Add blank line and remove blank space in profiles app admin.py

--- apr_breathwork/profiles/admin.py
+++ apr_breathwork/profiles/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import UserProfile
 
+
 class UserProfileAdmin(admin.ModelAdmin):
     """ Admin settings for Order model """
-    
+
     readonly_fields = ('paid_member_from', 'is_paid_member',)
 
     fields = (
