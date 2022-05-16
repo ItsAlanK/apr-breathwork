b7db065ee5bf9840fdc094536471b0572b3cb512
ItsAlanK <alankinsella1993@gmail.com>
2022-05-16 21:39:50 +0100

    Correct lines too long and indentation errors in info app views.py for pep8

--- apr_breathwork/info/views.py
+++ apr_breathwork/info/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.contrib import messages
 from .models import AboutUs
 
+
 def about(request):
     """ View which returns index page """
 
@@ -24,19 +25,21 @@ def contact(request):
         sender_email = request.POST.get('email')
         message = request.POST.get('message', '')
         email_subject = f'Message from {sender_name}'
-        email_body = ('APR Breathworks Contact form has received a new message.\n'
-        f'Message sent by {sender_name}, email address: {sender_email}.\n'
-        f'Message: {message}')
+        email_body = (
+            'APR Breathworks Contact form has received a new message.\n'
+            f'Message sent by {sender_name}, email address: {sender_email}.\n'
+            f'Message: {message}')
         email_sender = settings.DEFAULT_FROM_EMAIL
         email_recipient = settings.DEFAULT_FROM_EMAIL
 
-        send_mail(email_subject,
-        email_body, email_sender,
-        [email_recipient], fail_silently=False)
+        send_mail(
+            email_subject,
+            email_body, email_sender,
+            [email_recipient], fail_silently=False)
 
-        messages.success(request, 'Your message was sent. '
-        'We will be sure to get back to you soon!'
-        )
+        messages.success(
+            request, 'Your message was sent. '
+            'We will be sure to get back to you soon!')
         return redirect(reverse('home'))
 
     template = 'info/contact-us.html'
